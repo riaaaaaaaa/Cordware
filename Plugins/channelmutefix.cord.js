3ca8ac86fdabc58edf9b5da845599af52fd458c6
Yaekith <yaekith@gmail.com>
2020-10-24 11:19:16 +0800

    Create channelmutefix.cord.js

---
+++ Plugins/channelmutefix.cord.js
@@ -0,0 +1,24 @@
+var Plugin = require('./plugin');
+var CordAPI = require('../API/API');
+
+module.exports = new Plugin({
+    Name: "Channel Mute Fix",
+    Author: "Yaekith",
+    Description: "This plugin should fix the fact that you can get mentioned in muted channels .-.",
+    Version: 1.0,
+    OriginURL: "",
+    OnInjection: new function() {
+        var getMentionCount = CordAPI.Modding.FilterWebpackModule("getMentionCount");
+        CordAPI.Modding.PatchMethod(getMentionCount, 'getMentionCount', (result) => 
+        {
+            var isChannelMuted = CordAPI.Modding.FilterWebpackModule("isChannelMuted").isChannelMuted(result.methodArguments[0]);
+            var original = result.callOriginalMethod(result.methodArguments);
+            
+            if (isChannelMuted) {
+                original = 0;
+            }
+
+            return original;
+        });
+    }
+})

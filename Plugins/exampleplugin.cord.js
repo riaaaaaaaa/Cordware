921a82e7fcff9839d148a3931198c78098deced8
Yaekith <yaekith@gmail.com>
2020-10-25 08:07:58 +0800

    Create exampleplugin.cord.js

---
+++ Plugins/exampleplugin.cord.js
@@ -0,0 +1,27 @@
+var Plugin = require('./plugin');
+var CordAPI = require('../API/API');
+
+module.exports = new Plugin({
+    Name: "Example Plugin",
+    Author: "Example Bob",
+    Description: "An example plugin.",
+    Version: 1.0,
+    OriginURL: "",
+    OnInjection: function() {
+        //the function that gets called as soon as the plugin gets loaded, aka on Injection.
+
+        console.log('Hello from example plugin!');
+    },
+    OnEjection: function()
+    {
+        //the function that gets called when the client ejects or plugin disposes/unloads.
+
+        console.log('Farewell Cordware! This is goodbye from example plugin :(');
+
+        var audio = new Audio("https://cdn.discordapp.com/attachments/769417606523650088/769713693071376444/titanic.mp3");
+        audio.volume = 1.0;
+        audio.play();
+
+        //Rest in peace, example plugin, we'll miss you
+    }
+})

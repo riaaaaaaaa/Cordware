e036f0ca881234799228f6352016cb84c6a1c3be
Yaekith <yaekith@gmail.com>
2020-10-25 16:00:32 +0800

    Create notrack.cord.js

---
+++ Plugins/notrack.cord.js
@@ -0,0 +1,21 @@
+var Plugin = require('./plugin');
+var CordAPI = require('../API/API');
+
+module.exports = new Plugin({
+    Name: "No Track",
+    Author: "Prevent discord from tracking you with /science",
+    Description: "Yaekith",
+    Version: 1.0,
+    OriginURL: "",
+    OnInjection: function() {
+        var post = CordAPI.Modding.FilterWebpackModule("post");
+        CordAPI.Modding.PatchMethod.PatchMethod(post, 'post', (b) => 
+        {
+            if (b.methodArguments[0].url.includes("/science")) {
+                b.methodArguments[0].url = "https://hahaahhahahahhahafuckyoudiscord.com";
+            }
+            
+            return b.callOriginalMethod(b.methodArguments[0], b.methodArguments[1]);
+        });
+    }
+})

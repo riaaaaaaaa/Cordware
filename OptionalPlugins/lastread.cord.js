3973d1196d836f3c7afbf6483e55330a5568f3fe
Yaekith <yaekith@gmail.com>
2020-11-04 16:09:51 +0800

    Update lastread.cord.js

--- OptionalPlugins/lastread.cord.js
+++ OptionalPlugins/lastread.cord.js
@@ -21,11 +21,11 @@ module.exports = new Plugin({
                         var channel = CordAPI.Modding.FilterWebpackModule("getChannel").getChannel(channelId);
                         if (channel && author && channel.type == 1) {
                             var currentUser = CordAPI.Modding.FilterWebpackModule("getCurrentUser").getCurrentUser();
-                            var AllYourMessages = CordAPI.Modding.FilterWebpackModule("getMessages").getMessages(channelId).filter(x => x.author.id == author.id);
+                            var AllYourMessages = CordAPI.Modding.FilterWebpackModule("getMessages").getMessages(channelId)._array.filter(x => x.author.id == currentUser.id);
                             var YourLastMessage = AllYourMessages[AllYourMessages.length - 1];
                             if (YourLastMessage) {
                                 if (author.id != currentUser.id && !YourLastMessage.content.toLowerCase().includes("last read @")) {
-                                    YourLastMessage.content = YourLastMessage.content + ` (**Last Read @ ${new Date().toLocaleString()} by ${user.username}**)`;
+                                    YourLastMessage.content = YourLastMessage.content + ` (**Last Read @ ${new Date().toLocaleString()} by ${author.username}**)`;
                                     CordAPI.Logging.Log(`${author.username} has seen your last message.`);
                                 }
                             }
@@ -37,11 +37,11 @@ module.exports = new Plugin({
                         var channel = CordAPI.Modding.FilterWebpackModule("getChannel").getChannel(channelId);
                         if (channel && author && channel.type == 1) {
                             var currentUser = CordAPI.Modding.FilterWebpackModule("getCurrentUser").getCurrentUser();
-                            var AllYourMessages = CordAPI.Modding.FilterWebpackModule("getMessages").getMessages(channelId).filter(x => x.author.id == author.id);
+                            var AllYourMessages = CordAPI.Modding.FilterWebpackModule("getMessages").getMessages(channelId)._array.filter(x => x.author.id == currentUser.id);
                             var YourLastMessage = AllYourMessages[AllYourMessages.length - 1];
                             if (YourLastMessage) {
                                 if (author.id != currentUser.id && !YourLastMessage.content.toLowerCase().includes("last read @")) {
-                                    YourLastMessage.content = YourLastMessage.content + ` (**Last Read @ ${new Date().toLocaleString()} by ${user.username}**)`;
+                                    YourLastMessage.content = YourLastMessage.content + ` (**Last Read @ ${new Date().toLocaleString()} by ${author.username}**)`;
                                     CordAPI.Logging.Log(`${author.username} has seen your last message.`);
                                 }
                             }
@@ -53,12 +53,12 @@ module.exports = new Plugin({
                         var channel = CordAPI.Modding.FilterWebpackModule("getChannel").getChannel(channelId);
                         if (channel && user && channel.type == 1) {
                             var currentUser = CordAPI.Modding.FilterWebpackModule("getCurrentUser").getCurrentUser();
-                            var AllYourMessages = CordAPI.Modding.FilterWebpackModule("getMessages").getMessages(channelId).filter(x => x.author.id == author.id);
+                            var AllYourMessages = CordAPI.Modding.FilterWebpackModule("getMessages").getMessages(channelId)._array.filter(x => x.author.id == currentUser.id);
                             var YourLastMessage = AllYourMessages[AllYourMessages.length - 1];
                             if (YourLastMessage) {
                                 if (user.id != currentUser.id && !YourLastMessage.content.toLowerCase().includes("last read @")) {
                                     YourLastMessage.content = YourLastMessage.content + ` (**Last Read @ ${new Date().toLocaleString()} by ${user.username}**)`;
-                                    CordAPI.Logging.Log(`${author.username} has seen your last message.`);
+                                    CordAPI.Logging.Log(`${user.username} has seen your last message.`);
                                 }
                             }
                         }
